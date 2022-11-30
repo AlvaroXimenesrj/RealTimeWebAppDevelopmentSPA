851b8afa9d42c403390490b83b4fdad8a9c54517
alvaroximenesrj <alvaro.junior.ext@talentfour.com.br>
2022-11-30 00:37:48 -0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MainComponent } from './_shared/main/main.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  // {
+  //   path: 'user', component: UserComponent,
+  //   children: [
+  //     { path: 'login', component: LoginComponent }
+  //   ]
+  // },
+
+  {
+    path: 'dashboard', component: DashboardComponent,/* canActivate: [AuthGuard],*/
+    children: [
+      // { path: 'exportar', component: LeadExportarComponent, canActivate: [AuthGuard] },
+      // { path: 'pesquisa', component: LeadPesquisaComponent, canActivate: [AuthGuard] },
+      // { path: 'filtros', component: ComercialFiltrosComponent, canActivate: [AuthGuard] },
+    ]
+  },
+  {
+    path: 'home', component: MainComponent, /*canActivate: [AuthGuard],*/
+    children: [
+    ]
+  },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './_shared/main/main.component';
+import { NavBarComponent } from './_shared/nav-bar/nav-bar.component';
+import { HomeComponent } from './_shared/home/home.component';
+import { FooterComponent } from './_shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    MainComponent,
+    NavBarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
