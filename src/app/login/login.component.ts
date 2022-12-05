b9f163933d3b8217e7695f019d1cabaa0eebc7e5
alvaroximenesrj <alvaro.junior.ext@talentfour.com.br>
2022-12-05 19:35:54 -0300

    commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,83 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  public login: FormGroup
+  public showInvalidForm = 'hidden'
+  public disabledSavedButton = false
+  private baseUrl = environment.baseUrl
+
+  constructor(
+    private _http: HttpClient,
+    private _fb: FormBuilder
+  ) {
+    this.login = _fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      rememberMe: [false, [Validators.required]]
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  Login() {
+
+    if (this.login.valid) {
+      this.showInvalidForm = 'hidden'
+      this.disabledSavedButton = true
+
+      this._http.post(`${this.baseUrl}account/login`, this.login.value,
+        {
+
+        })
+        .subscribe({
+          next: (resp: any) => {
+            this.disabledSavedButton = false
+            // redirect
+          },
+          error: (fail: any) => {
+            this.disabledSavedButton = false
+          }
+        })
+    } else {
+      this.showInvalidForm = 'visible'
+    }
+
+  }
+
+  get invalidemail() {
+
+    if (this.showInvalidForm == 'visible' &&
+      (this.login.get('email')?.errors?.['required'] ||
+        this.login.get('email')?.errors?.['email'])) {
+
+      return 'visible'
+
+    } else {
+      return 'hidden'
+    }
+
+
+  }
+
+  get invalidPassword() {
+    if (this.showInvalidForm == 'visible' &&
+      (this.login.get('password')?.errors?.['required'] ||
+        this.login.get('password')?.errors?.['password'])) {
+
+      return 'visible'
+
+    } else {
+      return 'hidden'
+    }
+  }
+}
